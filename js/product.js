3bb0531bc7c50113c964233c54aedc5a2926cbbb
Xin-Yann <105412074+Xin-Yann@users.noreply.github.com>
2023-09-24 18:57:25 +0800

    Add files via upload

---
+++ js/product.js
@@ -0,0 +1,12 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+function openNav() {
+    document.getElementById("sideNav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
+
---
+++ js/user.js
@@ -0,0 +1,32 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+var myButton = document.getElementById("myButton");
+var closePopup = document.getElementById("closePopup");
+var myPopup = document.getElementById("myPopup");
+
+myButton.addEventListener("click", function () {
+    myPopup.classList.add("show");
+});
+
+closePopup.addEventListener("click", function () {
+    myPopup.classList.remove("show");
+});
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+           
+
