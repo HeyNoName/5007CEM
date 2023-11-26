c898115a9b09d8878c62b8f472ccf6d15f3fa422
Xin-Yann <105412074+Xin-Yann@users.noreply.github.com>
2023-11-26 17:31:55 +0800

    Add files via upload

--- js/product.js
+++ js/product.js
@@ -3,10 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 function openNav3() {
-    document.getElementById("sideNav").style.width = "350px";
+    document.getElementById("sideNav").style.width = "550px";
 }
 
 function closeNav3() {
     document.getElementById("sideNav").style.width = "0";
 }
-
+// function openNav3() {
+//    var modalContent = document.getElementById("modalContent");
+//    var productDetails = document.querySelector(".content");
+//
+//    // Set modal content dynamically
+//    document.getElementById("modalProductImage").src = productDetails.querySelector("img").src;
+//    document.getElementById("modalProductName").innerHTML = productDetails.querySelector("h3").innerHTML;
+//    document.getElementById("modalProductDescription").innerHTML = productDetails.querySelector(".content-title").innerHTML;
+//    document.getElementById("modalProductPrice").innerHTML = productDetails.querySelector("h2").innerHTML;
+//
+//    document.getElementById("sideNav").style.width = "100%";
+//}
+//
+//function closeNav3() {
+//    document.getElementById("sideNav").style.width = "0";
+//}
